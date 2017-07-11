5fdd1cd3bad8b31e0848d1f17f2d1e423ed2db12
Vinay Raikar <vinay.raikar@wipro.com>
2017-07-11 17:37:03 +0530

    Move runner function to lib to make it reusable

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const fs = require('fs');
 const rule1 = require('./rules/1-pass-non-zeros');
 const rule2 = require('./rules/2-occurrence-weights');
 const utils = require('./utils');
+const runner = require('./lib/runner');
 
 fs.readFile('./data/fe-jd.json', 'utf-8', function(err, jd) {
 	const jd_data = JSON.parse(jd);
-	let score;
+	let results;
 
-	fs.readFile('./data/cv-sample.txt', 'utf-8', function(err, cv_data) {
-		score = runner([rule1, rule2], cv_data.toLowerCase(), jd_data);
-		console.log(score);
+	fs.readFile('./data/cv-vinay.txt', 'utf-8', function(err, cv_data) {
+		results = runner([rule1, rule2], cv_data.toLowerCase(), jd_data);
+		console.log(results);
+    console.log( (utils.average(results.map((result)=>result.score))).toFixed(2) );
 	})
 
 });
@@ -18,12 +20,12 @@ fs.readFile('./data/fe-jd.json', 'utf-8', function(err, jd) {
 
 
 
-function runner(rules, cv, jd){
-	const scores = [];
-	rules.forEach((rule) => {
-		const { score, data } = rule(cv, jd);
-		console.log(data)
-		scores.push(score);
-	});
-	return (utils.average(scores)).toFixed(2);
-}
+// function runner(rules, cv, jd){
+// 	const scores = [];
+// 	rules.forEach((rule) => {
+// 		const { score, data } = rule(cv, jd);
+// 		console.log(data)
+// 		scores.push(score);
+// 	});
+// 	return (utils.average(scores)).toFixed(2);
+// }
