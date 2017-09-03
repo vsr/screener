fa8f20e5eba4856d4ae260a22c3a684a915c1b6d
Priya Ranjan Singh <mail@priyaranjan.net>
2017-09-03 08:51:34 +0530

    add unit test setup using mocha and chai

---
+++ test/canary.spec.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+
+describe('canary test', function() {
+  it('should always be true', function() {
+    expect(true).to.equal(true);
+  });
+})
